2f4937d8b8992743300da889cf23cffb0d525788
Michael Eden <deltt@outlook.de>
2025-10-02 19:38:46 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,10 +3,8 @@ const marker = document.getElementById('marker');
 
 marker.addEventListener('markerFound', () => {
     feedback.textContent = 'Marker detected!';
-    feedback.style.color = 'green';
 });
 
 marker.addEventListener('markerLost', () => {
     feedback.textContent = 'Point camera at marker';
-    feedback.style.color = '#fff';
 });
